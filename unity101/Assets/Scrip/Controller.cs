55f76e89c767f7fe815c6ab4a65c39755effb46a
Meatball906 <amu_1_0ve@hotmail.com>
2022-11-01 11:50:46 +0700

    updateController

---
+++ unity101/Assets/Scrip/Controller.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       print( transform.position);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  print("Hello");
+
+
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(0,transform.position.y + 0.001f, 0);
+               }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(0, transform.position.y + -0.001f, 0);
+        }
+
+
+
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x + 0.001f, 0, 0);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + -0.001f, 0, 0);
+        }
+
+
+
+    }
+}
